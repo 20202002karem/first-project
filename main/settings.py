e2d706afa94e8e4469927f53349839a50e4fecef
20202002karem <109469580+20202002karem@users.noreply.github.com>
2023-08-12 09:29:21 +0300

    update ci/cd pipeline

--- main/settings.py
+++ main/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7c*tv0d$f8_b3a*i0g1wt7(2b+*09r0!s+r#y#n=v-#_!ol*bd'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 ALLOWED_HOSTS = []
 if DEBUG is False:
-    ALLOWED_HOSTS = ['127.0.0.1']
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
 
 
